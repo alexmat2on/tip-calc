b94a35db36825049b3a2335131d5fa9165c96316
alexmat2on <alexmat2on@gmail.com>
2016-12-10 20:59:44 -0500

    add calculation

--- calculator.php
+++ calculator.php
@@ -1,8 +1,30 @@
-<html>
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Tip Calculator</title>
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
 <body>
-
-Your subtotal was <?php echo $_POST["subtotal"]; ?><br>
-Your tip percentage was: <?php echo $_POST["percentage"]; ?>
-
+  <h1> Tip Calculator </h1>
+  <form action="calculator.php" method="post"> Bill subtotal: $<input type="text" name="subtotal"><br><br>
+    Tip Percentage:<br>
+    <?php $percents = 10;
+      for ($x = 0; $x < 3; $x++) {
+      echo "<input type='radio' name='percentage' value='$percents'> $percents&#37;";
+      $percents += 5;
+    }
+    <br><br>
+    $tip = $_POST["subtotal"]*$_POST["percentage"]/100;
+    Tip: $tip;<br>
+    Total: echo $_POST["subtotal"] + $tip; ?>
+    <input type="submit" value="Submit">
 </body>
 </html>
